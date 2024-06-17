571e882536ca64aa6487ee2a9cdf7c31939960b8
Chase Williams <chasewilliams98@gmail.com>
2024-06-17 15:22:12 -0400

    Switch to Forge 1.20.1

--- src/main/java/org/occultechteam/occultech/client/keybindings/ModKeyBindings.java
+++
@@ -1,4 +0,0 @@
-package org.occultechteam.occultech.client.keybindings;
-
-public class ModKeyBindings {
-}
--- src/main/java/org/occultechteam/occultech/common/item/WandOfSparking.java
+++
@@ -1,9 +0,0 @@
-package org.occultechteam.occultech.common.item;
-
-import java.util.Properties;
-
-public class WandOfSparking {
-    public WandOfSparking(Properties props) {
-        super();
-    }
-}
