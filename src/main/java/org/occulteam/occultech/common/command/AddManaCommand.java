ebab30f15701922654a55e513b4859f3a1447159
Chase Williams <chasewilliams98@gmail.com>
2024-06-18 17:26:56 -0400

    Update AddManaCommand.java

--- src/main/java/org/occulteam/occultech/common/command/AddManaCommand.java
+++ src/main/java/org/occulteam/occultech/common/command/AddManaCommand.java
@@ -4,21 +4,18 @@ import org.occulteam.occultech.common.capability.CapabilityRegistry;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.DoubleArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.network.chat.ChatType;
-import net.minecraft.network.chat.OutgoingChatMessage;
-import net.minecraft.network.chat.PlayerChatMessage;
 import net.minecraft.world.entity.player.Player;
 
 public class AddManaCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("addmana")
-                .then(Commands.argument("amount", DoubleArgumentType.doubleArg(1, 1000)).executes(command -> {
-                    return execute(command, DoubleArgumentType.getDouble(command, "amount"));
+                .then(Commands.argument("amount", IntegerArgumentType.integer(1, 1000)).executes(command -> {
+                    return execute(command, IntegerArgumentType.getInteger(command, "amount"));
                 })));
     }
 
@@ -27,7 +24,8 @@ public class AddManaCommand {
             Player player = (Player) command.getSource().getEntity();
             if (player != null) { // Add null check for player variable
                 int curMaxMana = player.getCapability(CapabilityRegistry.MANA_CAPABILITY).resolve().get().getMaxMana();
-                player.getCapability(CapabilityRegistry.MANA_CAPABILITY).resolve().get().setMaxMana(curMaxMana + amount);
+                player.getCapability(CapabilityRegistry.MANA_CAPABILITY).resolve().get()
+                        .setMaxMana(curMaxMana + amount);
             }
         }
         return Command.SINGLE_SUCCESS;
