edc1a2dc85eb4dee55bd4a186f38a6d5fdd06a5f
Penguin-Hunter <neil.g314@gmail.com>
2024-06-18 23:20:59 -0400

    Partially set up GunItem

--- src/main/java/org/occulteam/occultech/common/item/GunItem.java
+++ src/main/java/org/occulteam/occultech/common/item/GunItem.java
@@ -1,7 +1,28 @@
 package org.occulteam.occultech.common.item;
 
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ProjectileWeaponItem;
+import org.jetbrains.annotations.NotNull;
+import org.occulteam.occultech.startup.ModItems;
 
-public class GunItem extends Item {
-    public GunItem() {super(new Item.Properties());}
+import java.util.function.Predicate;
+
+
+public class GunItem extends ProjectileWeaponItem {
+    public static final Predicate<ItemStack> BULLET_ONLY = (bul) -> {return bul.is(ModItems.BULLET.get());};
+
+
+    public GunItem(Item.Properties pProperties) {
+        super(pProperties);
+    }
+
+    public @NotNull Predicate<ItemStack> getAllSupportedProjectiles() {
+        return BULLET_ONLY;
+    }
+
+    @Override
+    public int getDefaultProjectileRange() {return 15;}
 }
