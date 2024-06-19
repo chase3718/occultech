7bda0e444b73177cbef89e7758a7eb73e8bdb251
Penguin-Hunter <neil.g314@gmail.com>
2024-06-19 18:19:28 -0400

    Fixed Merge Issues

--- src/main/java/org/occulteam/occultech/common/item/HandgunItem.java
+++ src/main/java/org/occulteam/occultech/common/item/HandgunItem.java
@@ -1,22 +1,31 @@
-// package org.occulteam.occultech.common.item;
+package org.occulteam.occultech.common.item;
 
-// import net.minecraft.sounds.SoundEvents;
-// import net.minecraft.sounds.SoundSource;
-// import net.minecraft.world.InteractionHand;
-// import net.minecraft.world.InteractionResultHolder;
-// import net.minecraft.world.entity.player.Player;
-// import net.minecraft.world.item.Item;
-// import net.minecraft.world.item.ItemStack;
-// import net.minecraft.world.level.Level;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.occulteam.occultech.common.item.GunItem;
+import net.minecraftforge.event.ForgeEventFactory;
 
-// public class HandgunItem extends GunItem {
-// public HandgunItem() {super(new Item.Properties());}
+public class HandgunItem extends GunItem {
+    private static final float shootingPower = 1.6F;
+    public HandgunItem() {super(new Item.Properties());}
 
-// public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer,
-// InteractionHand pUsedHand) {
-// pLevel.playSound(null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(),
-// SoundEvents.CROSSBOW_SHOOT, SoundSource.PLAYERS, 1.0F, 1.0F);
-// //if (pPlayer.getInventory().contains(BowItem)
-// return super.use(pLevel, pPlayer, pUsedHand);
-// }
-// }
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pHand);
+        if (!pPlayer.getProjectile(itemstack).isEmpty()) {
+            //performShooting(pLevel, pPlayer, pHand, itemstack, getShootingPower(itemstack), 1.0F);
+            pLevel.playSound((Player) null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.CROSSBOW_SHOOT, SoundSource.PLAYERS, 1.0F, 0.5F);
+            return InteractionResultHolder.consume(itemstack);
+        } else
+            return InteractionResultHolder.fail(itemstack);
+    }
+
+    public float getShootingPower(ItemStack iS) {return shootingPower;};
+
+    public int getDefaultProjectileRange() {return 15;};
+}
--- src/main/java/org/occulteam/occultech/startup/ModItems.java
+++ src/main/java/org/occulteam/occultech/startup/ModItems.java
@@ -2,6 +2,8 @@ package org.occulteam.occultech.startup;
 
 import org.occulteam.occultech.Occultech;
 import org.occulteam.occultech.common.item.WandItem;
+import org.occulteam.occultech.common.item.BulletItem;
+import org.occulteam.occultech.common.item.HandgunItem;
 
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -14,6 +16,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> FIREBALL_WAND = ITEMS.register("fireball_wand",
             () -> new WandItem(new Item.Properties(), 10, null, null, 10));
+
+    public static final RegistryObject<Item> BULLET = ITEMS.register("bullet", BulletItem::new);
+    public static final RegistryObject<Item> HANDGUN = ITEMS.register("handgun", HandgunItem::new);
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
