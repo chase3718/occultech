a9ee8bc623f2302c1c9fbcc2a0e378c2fa333203
Penguin-Hunter <neil.g314@gmail.com>
2024-06-18 22:12:24 -0400

    Added bullet

---
+++ src/main/java/org/occulteam/occultech/common/item/BulletItem.java
@@ -0,0 +1,7 @@
+package org.occulteam.occultech.common.item;
+
+import net.minecraft.world.item.Item;
+
+public class BulletItem extends Item {
+    public BulletItem() {super(new Item.Properties());};
+}
