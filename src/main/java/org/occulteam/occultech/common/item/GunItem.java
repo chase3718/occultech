1cc4cd8569fd2417126831f11a085b33ee85fd3c
Penguin-Hunter <neil.g314@gmail.com>
2024-06-19 20:46:43 -0400

    Added gun reloading
    Need to change system to use NBT

--- src/main/java/org/occulteam/occultech/common/item/GunItem.java
+++ src/main/java/org/occulteam/occultech/common/item/GunItem.java
@@ -6,6 +6,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.SmallFireball;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -19,11 +20,22 @@ import java.util.function.Predicate;
 public abstract class GunItem extends ProjectileWeaponItem {
     public static final Predicate<ItemStack> BULLET_ONLY = (bul) -> bul.is(ModItems.BULLET.get());
 
-    private int maxAmmo = 1;
-    private int curAmmo = 0;
+    private int maxAmmo;
+    private int curAmmo;
+    private float shootingPower;
 
-    public GunItem(Item.Properties pProperties) {
+    public GunItem(Item.Properties pProperties, int pMaxAmmo, float pShootingPower) {
         super(pProperties);
+        maxAmmo = pMaxAmmo;
+        shootingPower = pShootingPower;
+        curAmmo = 0;
+    }
+
+    public void fire(Level pLevel, Player pPlayer) {
+        pLevel.playSound((Player) null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.CROSSBOW_SHOOT, SoundSource.PLAYERS, 1.0F, 1.0F);
+        curAmmo--;
+        pPlayer.sendSystemMessage(Component.literal(String.valueOf(curAmmo)));
+        SmallFireball smallFireball = new SmallFireball(pLevel, pPlayer, pPlayer.getXRot(), pPlayer.getYRot(), 0);
     }
 
     public Predicate<ItemStack> getAllSupportedProjectiles() {
@@ -42,25 +54,40 @@ public abstract class GunItem extends ProjectileWeaponItem {
         return curAmmo;
     }
 
+    public void setMaxAmmo(int ammo) {
+        maxAmmo = Math.max(1, ammo);
+    }
+
+    public int getMaxAmmo() {
+        return maxAmmo;
+    }
+
+    public void consumeAmmo() {
+        curAmmo = Math.max(0, curAmmo-1);
+    }
+
+    public float getShootingPower() {
+        return shootingPower;
+    }
+
     public boolean loadAmmo (Level pLevel, Player pPlayer, InteractionHand pHand) {
         ItemStack itemstack = pPlayer.getItemInHand(pHand);
         if (!pPlayer.getProjectile(itemstack).isEmpty()) {
             Predicate<ItemStack> predicate = ((ProjectileWeaponItem)itemstack.getItem()).getAllSupportedProjectiles();
-            ItemStack ammoitemstack = null;
+            ItemStack ammoItemStack = null;
             for(int i = 0; i < pPlayer.getInventory().getContainerSize(); ++i) {
                 ItemStack itemstack1 = pPlayer.getInventory().getItem(i);
                 if (predicate.test(itemstack1)) {
-                    ammoitemstack = pPlayer.getInventory().getItem(i);
+                    ammoItemStack = pPlayer.getInventory().getItem(i);
                     break;
                 }
             }
-            if (ammoitemstack == null) {
-                pLevel.playSound((Player) null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.DISPENSER_FAIL, SoundSource.PLAYERS, 1.0F, 1.0F);
+            if (ammoItemStack == null) {
+                pPlayer.displayClientMessage(Component.literal("Oops! There was an issue reloading!"), true);
                 return false;
             } else {
-                int reloadct = (int) Math.min(maxAmmo, ammoitemstack.getCount());
-                pPlayer.displayClientMessage(Component.literal(String.valueOf(reloadct)), true);
-                ammoitemstack.split(reloadct);
+                int reloadct = (int) Math.min(maxAmmo, ammoItemStack.getCount());
+                ammoItemStack.split(reloadct);
                 curAmmo = reloadct;
                 pLevel.playSound((Player) null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.CROSSBOW_LOADING_END, SoundSource.PLAYERS, 1.0F, 1.0F);
                 return true;
--- src/main/java/org/occulteam/occultech/common/item/HandgunItem.java
+++ src/main/java/org/occulteam/occultech/common/item/HandgunItem.java
@@ -5,6 +5,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.Snowball;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ProjectileWeaponItem;
@@ -15,24 +16,19 @@ import net.minecraftforge.event.ForgeEventFactory;
 import java.util.function.Predicate;
 
 public class HandgunItem extends GunItem {
-    private static final float shootingPower = 1.6F;
     public HandgunItem() {
-        super(new Item.Properties());
-        //maxAmmo = 5;
+        super(new Item.Properties(), 5, 1.6F);
     }
 
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
-//        ItemStack itemstack = pPlayer.getItemInHand(pHand);
-//        if (!pPlayer.getProjectile(itemstack).isEmpty()) {
-//            pLevel.playSound((Player) null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.CROSSBOW_SHOOT, SoundSource.PLAYERS, 1.0F, 0.5F);
-//            return InteractionResultHolder.consume(itemstack);
-//        } else
-//            return InteractionResultHolder.fail(itemstack);
-        loadAmmo(pLevel, pPlayer, pHand);
-        return InteractionResultHolder.consume(pPlayer.getItemInHand(pHand));
+        if (!pLevel.isClientSide) {
+            if (getCurAmmo() != 0) {
+                fire(pLevel, pPlayer);
+            } else
+                loadAmmo(pLevel, pPlayer, pHand);
+        }
+        return InteractionResultHolder.fail(pPlayer.getItemInHand(pHand));
     }
-
-    public float getShootingPower() {return shootingPower;};
 
     public int getDefaultProjectileRange() {return 15;};
 }
