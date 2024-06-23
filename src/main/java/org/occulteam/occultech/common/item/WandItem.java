be64b41194b88e5a90557aaff7e387b0ad527aa0
Chase Williams <chasewilliams98@gmail.com>
2024-06-23 10:36:21 -0400

    Wand updates

--- src/main/java/org/occulteam/occultech/common/item/WandItem.java
+++ src/main/java/org/occulteam/occultech/common/item/WandItem.java
@@ -1,21 +1,22 @@
 package org.occulteam.occultech.common.item;
 
-import javax.swing.text.html.parser.Entity;
+import java.lang.reflect.Constructor;
+import java.util.function.Supplier;
 
 import org.jetbrains.annotations.NotNull;
 import org.occulteam.occultech.common.capability.mana.CapRegistry;
 import org.occulteam.occultech.common.capability.mana.IMana;
 
-import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.OutgoingChatMessage;
-import net.minecraft.network.chat.PlayerChatMessage;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.monster.Blaze;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -23,15 +24,18 @@ import net.minecraft.world.level.Level;
 public class WandItem extends Item {
     public int MANA_COST;
     public SoundEvent SOUND;
-    public Entity PROJECTILE;
+    public Supplier<? extends Projectile> PROJECTILE;
     public int COOLDOWN;
 
-    public WandItem(Item.Properties properties, int manaCost, SoundEvent sound, Entity projectile, int cooldown) {
+    public WandItem(Item.Properties properties, int manaCost, SoundEvent sound,
+            Supplier<? extends Projectile> projectile,
+            int cooldown) {
         super(properties);
         MANA_COST = manaCost;
         SOUND = sound;
         PROJECTILE = projectile;
         COOLDOWN = cooldown;
+
     }
 
     public int getManaCost() {
@@ -56,7 +60,6 @@ public class WandItem extends Item {
             return InteractionResultHolder.pass(stack);
         }
 
-        player.getCooldowns().addCooldown(this, COOLDOWN);
         IMana mana = CapRegistry.getMana(player).orElse(null);
 
         if (mana == null) {
@@ -68,14 +71,23 @@ public class WandItem extends Item {
             player.displayClientMessage(manaMsg, false);
             return InteractionResultHolder.fail(stack);
         }
+        player.getCooldowns().addCooldown(this, COOLDOWN);
 
         mana.removeMana(MANA_COST);
 
         level.playSound(null, player.getX(), player.getY(), player.getZ(), SOUND, SoundSource.PLAYERS, 1.0F, 1.0F);
-
         player.awardStat(Stats.ITEM_USED.get(this));
         player.startUsingItem(hand);
+        fireProjectile(level, player);
         return InteractionResultHolder.consume(stack);
     }
 
+    private void fireProjectile(Level level, Player player) {
+        try {
+        } catch (Exception e) {
+            CapRegistry.getMana(player).ifPresent(mana -> mana.addMana(MANA_COST));
+            player.displayClientMessage(Component.literal(e.toString()), false);
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/occulteam/occultech/startup/ModItems.java
+++ src/main/java/org/occulteam/occultech/startup/ModItems.java
@@ -6,6 +6,7 @@ import org.occulteam.occultech.common.item.BulletItem;
 import org.occulteam.occultech.common.item.RevolverItem;
 
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.projectile.SmallFireball;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,7 +17,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Occultech.MODID);
 
     public static final RegistryObject<Item> FIREBALL_WAND = ITEMS.register("fireball_wand",
-            () -> new WandItem(new Item.Properties(), 10, SoundEvents.BLAZE_SHOOT, null, 10));
+            () -> new WandItem(new Item.Properties(), 150, SoundEvents.BLAZE_SHOOT, SmallFireball::new, 10));
 
     public static final RegistryObject<Item> BULLET = ITEMS.register("bullet", BulletItem::new);
     public static final RegistryObject<Item> REVOLVER = ITEMS.register("revolver", RevolverItem::new);
